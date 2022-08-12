212886ede342b20d4ba67cf042a799d09c8b2bb1
AleksandrPopkov <111053390+AleksandrPopkov@users.noreply.github.com>
2022-08-12 08:40:38 +0400

    Update and rename Task3.java to Task4.java

--- Task4.java
+++ Task4.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Task4 {
+//    Напишите метод, который будет вычислять угол между часовой и минутной стрелками часов.
+//    На вход функции подаётся время в виде двух переменных: "hours" и "minutes"
+//    Расчет угла производить относительно реального поведения стрелок часов.
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         System.out.print("write the number of the hour hand: ");
